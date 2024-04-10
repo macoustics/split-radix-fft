4f35e370750a3e52f1d99e7a79bab51ac3f32f69
Niels de Koeijer <nemk@bang-olufsen.dk>
2024-04-10 15:59:19 +0200

    build

---
+++ build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Shell
+shell() {
+    docker run \
+        -it \
+        -v $(pwd):/workspace/ \
+        split-radix-fft-dev:latest \
+        $@
+}
+
+# Function to perform a standard build
+build_normal() {
+    echo "Performing a normal build..."
+    mkdir -p .build
+    shell cmake \
+            -B .build \
+            -G Ninja \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DBUILD_TESTS_SPLIT_RADIX_FFT=ON
+    shell cmake --build .build
+}
+
+# Function to run tests
+run_tests() {
+    echo "Running tests..."
+    shell ./.build/tests/tests
+}
+
+# Function to build Docker image
+build_docker() {
+    echo "Building Docker image..."
+    docker build -t split-radix-fft-dev:latest .
+}
+
+# Check if no argument is provided
+if [ $# -eq 0 ]; then
+    build_normal
+    exit 0
+fi
+
+# Parse command line options
+while getopts ":tds" opt; do
+    case ${opt} in
+        t )
+            build_normal
+            run_tests
+            ;;
+        d )
+            build_docker
+            ;;
+        s )
+            shell
+            ;;
+        \? )
+            echo "Usage: cmd [-t] for tests, [-d] for docker build, no option for normal build"
+            exit 1
+            ;;
+    esac
+done
+
